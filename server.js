bbd72e2da556ba9911fc5f80eb250702653ae951
David M <david.munoz1994@gmail.com>
2018-09-11 17:56:11 -0400

    Updating todos, PUT Postman

--- server.js
+++ server.js
@@ -53,6 +53,45 @@ app.post('/todos', function (req, res) {
 });
 
 // DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoID = parseInt (req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+	 	res.json(matchedTodo);
+	 }
+});
+
+// PUT /todos/:id
+app.put('/todos:/id', function (req, res) {
+	var todoID = parseInt (req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	} 
+	//if the 404 runs, the code below does not. ! is the not operator.
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} 
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '!');

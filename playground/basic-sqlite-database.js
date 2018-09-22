fe929d8ebe94fb9a38857a9cbdf96e87b90a2929
D4V31994 <david.munoz1994@gmail.com>
2018-09-22 16:49:35 -0400

    Add todo model and POST /todos

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -24,7 +24,7 @@ sequelize.sync({
 }).then(function() {
 	console.log('Everything is synced');
 
-	Todo.findById(4).then(function (todo) {
+	Todo.findById(2).then(function (todo) {
 		if (todo) {
 			console.log(todo.toJSON());
 		} else {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -61,21 +62,26 @@ app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	// Use _.pick to only pick description and completed
 
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	//set body.desciption to be trimmed value
-	body.description = body.description.trim();
+	// //set body.desciption to be trimmed value
+	// body.description = body.description.trim();
 
-	// add id field, increment  +1 after
-	body.id = todoNextId++;
+	// // add id field, increment  +1 after
+	// body.id = todoNextId++;
 
-	// push body into array
-	todos.push(body);
+	// // push body into array
+	// todos.push(body);
 
-	res.json(body);
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -125,6 +131,8 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
